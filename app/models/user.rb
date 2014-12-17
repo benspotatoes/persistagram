bf7beeca02e16e57997eeef59dba473cbc4a87d2
Benjamin Hsieh <ben@benspotatoes.com>
2014-12-17 11:29:43 -0800

    Add nil check for new syncs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   SYNC_LIMIT_DAYS = 7
 
   def can_sync?
+    return true if last_sync.nil?
+
     since_last = ((Time.now - last_sync) / 86400).round
     return since_last > SYNC_LIMIT_DAYS.days, SYNC_LIMIT_DAYS - since_last
   end
