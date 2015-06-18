3b971dbce2d2e702d6116b5483d42ab9c45a3546
Benjamin Hsieh <ben@benspotatoes.com>
2015-06-17 17:50:32 -0700

    Remove snake case funcs

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type InstagramMedia struct {
   MediaId string `json:"media_id"`
 }
 
-func get_ig_access_token() oauth2.Token {
+func getIgAccessToken() oauth2.Token {
   conf := &oauth2.Config{
     ClientID: os.Getenv("IG_CLIENT_ID"),
     ClientSecret: os.Getenv("IG_CLIENT_SECRET"),
@@ -46,7 +46,7 @@ func get_ig_access_token() oauth2.Token {
   return *token
 }
 
-func get_ig_source(src string, tok string) (author, source string) {
+func getIgSource(src string, tok string) (author, source string) {
   resp, err := http.Get(fmt.Sprintf("http://api.instagram.com/oembed?url=%s", src))
   if err != nil {
     log.Fatal(err)
@@ -103,19 +103,19 @@ func get_ig_source(src string, tok string) (author, source string) {
   return author, source
 }
 
-func get_ig_filename(path string) string {
+func getIgFilename(path string) string {
   re := regexp.MustCompile("\\w+\\.(mp4|jpg|png)")
   filename := re.FindString(path)
 
   if os.Getenv("DEBUG") == "true" {
-    fmt.Printf("[debug] get_ig_filename: path: %s, filename: %s\n", path, filename)
+    fmt.Printf("[debug] getIgFilename: path: %s, filename: %s\n", path, filename)
   }
 
   return filename
 }
 
-func save_source_to_dbox(author string, source string, client *dropbox.Dropbox) {
-  filename := get_ig_filename(source)
+func saveSourceToDbox(author string, source string, client *dropbox.Dropbox) {
+  filename := getIgFilename(source)
   local := fmt.Sprintf("/tmp/%s", filename)
 
   // // https://github.com/thbar/golang-playground/blob/master/download-files.go
@@ -150,7 +150,7 @@ func main() {
   access_token = string(ig_access_token)
 
   if access_token == "" {
-    token := get_ig_access_token()
+    token := getIgAccessToken()
     access_token = token.AccessToken
     err = ioutil.WriteFile("/tmp/ig_access_token", []byte(access_token), 0644)
     if err != nil {
@@ -176,8 +176,8 @@ func main() {
     if url != "" {
       fmt.Printf("Saving URL: %s\n", url)
 
-      author, source := get_ig_source(url, access_token)
-      save_source_to_dbox(author, source, db)
+      author, source := getIgSource(url, access_token)
+      saveSourceToDbox(author, source, db)
     }
   }
 }
