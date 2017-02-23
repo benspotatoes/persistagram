51258209d57576e9882107147727acff77d812f8
Benjamin Hsieh <bhsieh@twitch.tv>
2017-02-23 14:48:48 -0800

    Update parser to read shared data for gallery images

--- api/media.go
+++ api/media.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -22,12 +23,36 @@ var (
 	jpgRe = regexp.MustCompile(`og:image" content="(https:\/\/.*\.jpg)`)
 	ctnRe = regexp.MustCompile(`og:description" content=".* \(@(\w+)\)`)
 	mdaRe = regexp.MustCompile(`meta name="medium" content="(.*)"`)
+
+	sharedDataRe = regexp.MustCompile(`sharedData = (\{.*\})`)
 )
 
 const (
 	retryCount    = 5
 	retryDuration = 30 * time.Second
 )
+
+type SharedData struct {
+	EntryData *EntryData `json:"entry_data"`
+}
+type EntryData struct {
+	PostPage []*PostPage `json:"PostPage"`
+}
+type PostPage struct {
+	Media *Media `json:"media"`
+}
+type Media struct {
+	EdgeSidecarToChildren *EdgeSidecarToChildren `json:"edge_sidecar_to_children"`
+}
+type EdgeSidecarToChildren struct {
+	Edges []*Edge `json:"edges"`
+}
+type Edge struct {
+	Node *Node `json:"node"`
+}
+type Node struct {
+	DisplayUrl string `json:"display_url"`
+}
 
 func (rt *Router) saveLiked(c web.C, w http.ResponseWriter, r *http.Request) {
 	err := rt.Dropbox.DownloadToFile(rt.Config.LikedTxtPath, "/tmp/liked.txt.tmp", "")
@@ -71,54 +96,110 @@ func (rt *Router) saveLiked(c web.C, w http.ResponseWriter, r *http.Request) {
 		}
 		username := meta[1]
 
-		metadata := mdaRe.FindStringSubmatch(sbody)
-		if len(metadata) != 2 {
-			log.Println("unable to get medium type")
-			continue
-		}
-		mediaType := metadata[1]
+		var src []string
 
-		var src string
-		switch mediaType {
-		case "image":
-			img := jpgRe.FindStringSubmatch(sbody)
-			if len(img) != 2 {
-				log.Println("unable to get image source")
+		sharedData := sharedDataRe.FindStringSubmatch(sbody)
+		if len(sharedData) != 2 {
+			log.Println("unable to get shared media, falling back to basic parsing")
+
+			metadata := mdaRe.FindStringSubmatch(sbody)
+			if len(metadata) != 2 {
+				log.Println("unable to get medium type")
 				continue
 			}
-			src = img[1]
-		case "video":
-			vid := mp4Re.FindStringSubmatch(sbody)
-			if len(vid) != 2 {
-				log.Println("unable to get video source")
-				continue
-			}
-			src = vid[1]
-		default:
-			continue
-		}
+			mediaType := metadata[1]
 
-		data := backend.InstagramMetadata{
-			Author:   username,
-			Source:   src,
-			Filename: getMediaFilename(src),
-		}
-		log.Println(data)
-
-		go func() {
-			retry := 0
-			for {
-				err := backend.SaveMedia(data, rt.Dropbox)
-				if err != nil {
-					if retry >= retryCount {
-						log.Printf("error saving media for url %s: %q\n", data.Source, err)
-						return
-					}
-					time.Sleep(time.Duration(retry) * retryDuration)
+			switch mediaType {
+			case "image":
+				img := jpgRe.FindStringSubmatch(sbody)
+				if len(img) != 2 {
+					log.Println("unable to get image source")
+					continue
 				}
-				return
+				src = append(src, img[1])
+			case "video":
+				vid := mp4Re.FindStringSubmatch(sbody)
+				if len(vid) != 2 {
+					log.Println("unable to get video source")
+					continue
+				}
+				src = append(src, vid[1])
+			default:
+				continue
 			}
-		}()
+		} else {
+			blob := sharedData[1]
+			data := SharedData{}
+			err := json.Unmarshal([]byte(blob), &data)
+			if err != nil {
+				log.Printf("unable to unmarshal shared data: %q", err)
+				continue
+			}
+			entryData := data.EntryData
+			if entryData == nil {
+				log.Println("empty entry data")
+				continue
+			}
+			postPage := entryData.PostPage
+			if len(postPage) == 0 {
+				log.Println("empty post page")
+				continue
+			}
+			for _, post := range data.EntryData.PostPage {
+				media := post.Media
+				if media == nil {
+					log.Println("empty post media")
+					continue
+				}
+				sidecar := media.EdgeSidecarToChildren
+				if sidecar == nil {
+					log.Println("empty edge sidecar to children")
+					continue
+				}
+				edges := sidecar.Edges
+				if len(edges) == 0 {
+					log.Println("empty sidecar edges")
+					continue
+				}
+				for _, edge := range edges {
+					node := edge.Node
+					if node == nil {
+						log.Println("empty edge node")
+						continue
+					}
+					displayURL := node.DisplayUrl
+					if displayURL == "" {
+						log.Println("empty display url")
+						continue
+					}
+					src = append(src, displayURL)
+				}
+			}
+		}
+
+		for _, s := range src {
+			data := backend.InstagramMetadata{
+				Author:   username,
+				Source:   s,
+				Filename: getMediaFilename(s),
+			}
+			log.Println(data)
+
+			go func() {
+				retry := 0
+				for {
+					err := backend.SaveMedia(data, rt.Dropbox)
+					if err != nil {
+						if retry >= retryCount {
+							log.Printf("error saving media for url %s: %q\n", data.Source, err)
+							return
+						}
+						time.Sleep(time.Duration(retry) * retryDuration)
+					}
+					return
+				}
+			}()
+		}
 	}
 	_, err = rt.Dropbox.Delete(rt.Config.LikedTxtPath)
 	if err != nil {
