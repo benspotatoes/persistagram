1eddd49802055f6a05a5f6f0f17c4955c610a5b8
Benjamin Hsieh <bhsieh@twitch.tv>
2017-04-17 17:24:22 -0700

    Experiment with description regexp

--- api/media.go
+++ api/media.go
@@ -21,7 +21,7 @@ var (
 
 	mp4Re = regexp.MustCompile(`og:video:secure_url" content="(https:\/\/.*\.mp4)" `)
 	jpgRe = regexp.MustCompile(`og:image" content="(https:\/\/.*\.jpg)`)
-	ctnRe = regexp.MustCompile(`og:description" content=".* \(@(.*)\) on Instagram`)
+	ctnRe = regexp.MustCompile(`og:description" content=".* \(?@(.*)\)? on Instagram`)
 	mdaRe = regexp.MustCompile(`meta name="medium" content="(.*)"`)
 
 	sharedDataRe = regexp.MustCompile(`sharedData = (\{.*\})`)
